ced2044997c4fc99e52e3b616aad59a6b41f4a73
Mukkara <DT239877@dstsystems.com>
2023-11-13 10:22:41 +0530

    episode-1 react

--- App.js
+++ App.js
@@ -12,15 +12,15 @@
     </div> */
 
 
-const parent = React.createElement(
+    const parent = React.createElement(
 
-    "div",
-    {id:"parent"},
-    React.createElement(
         "div",
-        {id:"Child"},
-       [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]
-    )
-);
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(parent);
+        {id:"parent"},
+        React.createElement(
+            "div",
+            {id:"Child"},
+           [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]
+        )
+    );
+        const root = ReactDOM.createRoot(document.getElementById("root"));
+        root.render(parent);
